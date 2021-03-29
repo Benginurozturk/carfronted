ec6b1c26cb5d9daa1b798716534794643a73eadc
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-03-29 23:27:46 +0300

    hesap tutarı butonu eklendi

--- src/app/components/pages/admin-dashboard-page/cars-dashboard/car-edit-form/car-edit-form.component.ts
+++ src/app/components/pages/admin-dashboard-page/cars-dashboard/car-edit-form/car-edit-form.component.ts
@@ -64,7 +64,7 @@ export class CarEditFormComponent implements OnInit {
 
   createCarForm() {
     this.carEditForm = this.formBuilder.group({
-      name: [this.car.description, Validators.required],
+      //name: [this.car.description, Validators.required],
       brandId: [this.car.brandID, Validators.required],
       colorId: [this.car.colorID, Validators.required],
       dailyPrice: [this.car.dailyPrice, Validators.required],
